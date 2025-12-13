9608b5dd48e26b68e0b75f6727ec09db9ca3fb68
chiri <chirik2000xd@mail.ru>
2025-12-13 14:40:53 +0800

    Make Homework 35.2

--- materials/urls.py
+++ materials/urls.py
@@ -16,7 +16,7 @@ router.register("course", CourseViewSet, basename="course")
 urlpatterns = [
     path("subscription/", SubscriptionAPIView.as_view(), name="subscription"),
     path("lesson/", LessonListAPIView.as_view(), name="lessons_list"),
-    path("lesson/create", LessonCreateAPIView.as_view(), name="lesson_create"),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson_create"),
     path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson_detail"),
     path("lesson/<int:pk>/update", LessonUpdateAPIView.as_view(), name="lesson_update"),
     path(
